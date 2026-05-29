f318252e9d26d825655cef009afa3239627ef76e
ewu <erya.wuu@gmail.com>
2026-05-29 18:09:05 +0200

    feat: helath check route.

---
+++ ft_transcendence/services/user-service/src/routes/health.routes.ts
@@ -0,0 +1,10 @@
+import type{ FastifyInstance} from "fastify";
+
+export async function healthCheckRoutes(fastify: FastifyInstance) {
+    fastify.get(
+        "/health",
+        async () => {
+            return { service: "user-service", status: "healthy" };
+        },
+    );
+};
