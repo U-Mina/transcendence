cf3825207fdc14a340167c8023b6fb7a28707942
ewu <erya.wuu@gmail.com>
2026-05-30 11:49:30 +0200

    feat: mock data structure of internal user and frontend user-profile.

--- ft_transcendence/services/user-service/src/users.types.ts
+++ ft_transcendence/services/user-service/src/users.types.ts
@@ -2,3 +2,18 @@
  * the interface of users data type
  * now for mocking usage
  */
+
+// internal user interface with all necessary data
+export interface UserDTO {
+    id: string;
+    intraName: string;
+    email: string;
+    intraUrl: string;
+    createdAt: string;
+}
+
+// frontend visible user's data
+export interface UserProfile {
+    intraName: string;
+    intraUrl: string;
+}
