614b6b0ca0a14a1d596e2c661b3c6a5ed607f7b9
ewu <erya.wuu@gmail.com>
2026-06-13 14:28:38 +0200

    feat: for easier transfer to real db, use data repo now.

---
+++ ft_transcendence/services/event-service/src/event.repository.ts
@@ -0,0 +1,87 @@
+/**
+ * for a easier adaption to real data base
+ * for CRUD ops
+ */
+import { InternalEventEntity } from "./event.types"
+
+class EventRepository {
+    // mock data, make c++ style, in class private
+    private mockEvents: InternalEventEntity[] = [
+        {
+            eventId: "www",
+            eventName: "event-01",
+            startTime: new Date("2001-01-01"),
+            endTime: new Date("2001-01-02"),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            description: "test event 01",
+            creatorId: "hsajkkdka",
+            safetyCheck: false,
+        },
+        {
+            eventId: "eee",
+            eventName: "event-02",
+            startTime: new Date("2001-01-02"),
+            endTime: new Date("2001-01-03"),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            description: "test event 02",
+            creatorId: "jasdhla",
+            safetyCheck: false,
+        },
+        {
+            eventId: "rrr",
+            eventName: "event-03",
+            startTime: new Date("2001-01-03"),
+            endTime: new Date("2001-01-05"),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            description: "test event 03",
+            creatorId: "kshdaa",
+            safetyCheck: true,
+        }
+    ];
+    
+    // get all
+   async getAll() {
+    return this.mockEvents;
+   } 
+
+    // get by id
+    async getEventById(eventId: string): Promise<InternalEventEntity | undefined> {
+        const event = this.mockEvents.find(eve => eve.eventId === eventId);
+        if (!event) {
+            return undefined;
+        }
+        return event;
+    }
+
+    // create event (push new to repo.ts)
+    // NOTE: using return type here bc it easier to replace with explict cintract
+    async createEvent(event: InternalEventEntity): Promise<InternalEventEntity | undefined> {
+        this.mockEvents.push(event);
+        return event;
+    }
+
+    // update event (update)
+    async updateEvent(eventId: string, updatedEvent: InternalEventEntity): Promise<InternalEventEntity | undefined> {
+       const index = this.mockEvents.findIndex(eve => eve.eventId === eventId);
+       if (index === -1) {
+        return undefined;
+       }
+       this.mockEvents[index] = updatedEvent;
+       return updatedEvent;
+    }
+
+    // delete (popout from repo.ts), here can return full event-data, but this will be too waste of mem
+    async deleteEvent(eventId: string): Promise<boolean> {
+        const index = this.mockEvents.findIndex(eve => eve.eventId === eventId);
+        if (index === -1) {
+            return false;
+        }
+        this.mockEvents.splice(index, 1);
+        return true;
+    }
+}
+
+export const eventRepository = new EventRepository();
