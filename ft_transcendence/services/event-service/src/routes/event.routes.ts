c2d07fbcb5fced90b401c20261d1596cecd808ba
ewu <erya.wuu@gmail.com>
2026-06-13 15:30:39 +0200

    feat: finish update routes.

--- ft_transcendence/services/event-service/src/routes/event.routes.ts
+++ ft_transcendence/services/event-service/src/routes/event.routes.ts
@@ -2,30 +2,33 @@
  * routes for event-service
  */
 import { type FastifyInstance, type FastifyRequest, type FastifyReply } from "fastify";
-import { eventService } from "../services/event.service";
+import { EventService } from "../services/event.service";
 import { CreateEventDTO, UpdateEventDTO } from "../event.types"
 
 export async function EventServiceRoutes(fastify: FastifyInstance) {
     // get event by id
     // the home page is /home, then on /home/events will go event board
     fastify.get(
-        "events/:eventId",
+        "/events/:eventId",
         async (
             request: FastifyRequest<{
                 Params: { eventId: string }
             }>,
             reply: FastifyReply,
         ) => {
-            // a dummy userId for now, later will be: request.user.id
-            const userId = request.headers["dummy-userId-before-JWT"] as string;
-            const { eventId } = request.params;
-            const event = await eventService.getEventById(userId, eventId);
-
-            if (!event) {
-                return reply.status(404).send({ error: "Event not found." });
+            try {
+                // a dummy userId for now, later will be: request.user.id
+                const userId = request.headers["dummy-userId-before-JWT"] as string;
+                const { eventId } = request.params;
+                const event = await EventService.getEventById(userId, eventId);
+    
+                if (!event) {
+                    return reply.status(404).send({ error: "Event not found." });
+                }
+                return reply.status(200).send(event);
+            } catch (error) {
+                return reply.status(500).send({ error: "Fail to get event." });
             }
-
-            return reply.status(200).send(event);
         },
     );
 
@@ -36,12 +39,16 @@ export async function EventServiceRoutes(fastify: FastifyInstance) {
             _: FastifyRequest,
             reply: FastifyReply,
         ) => {
-            const events = await eventService.getAllEvents();
-            // if (!eves || eves.length === 0) {
-            //     return reply.status(404).send({ error: "Events not found. " });
-            // }
-            // even on empty, the get all event is still valid!
-            return reply.status(200).send(events || []);
+            try {
+                const events = await EventService.getAllEvents();
+                // if (!eves || eves.length === 0) {
+                //     return reply.status(404).send({ error: "Events not found. " });
+                // }
+                // even on empty, the get all event is still valid!
+                return reply.status(200).send(events || []);
+            } catch (error) {
+                return reply.status(500).send({ error: "Fail to fetch events." });
+            }
         },
     );
 
@@ -54,11 +61,41 @@ export async function EventServiceRoutes(fastify: FastifyInstance) {
             }>,
             reply: FastifyReply,
         ) => {
-            const newEvent = await eventService.createEvent(request.body);
-            if (!newEvent) {
-                return reply.status(500).send({ error: "Fail to create new Event." });
+            try {
+                const newEvent = await EventService.createEvent(request.body);
+                if (!newEvent) {
+                    return reply.status(500).send({ error: "Fail to create new Event." });
+                }
+                return reply.status(200).send(newEvent);
+            } catch (error) {
+                return reply.status(500).send({ error: "Fail to create new event." });
             }
-            return reply.status(200).send(newEvent);
+        },
+    );
+
+    // update an existing event
+    fastify.put(
+        "/events/:eventId",
+        async (
+            request: FastifyRequest<{
+                Params: {
+                    eventId: string,
+                    userId: string,
+                },
+                Body: UpdateEventDTO,
+            }>,
+            reply: FastifyReply,
+        ) => {
+            try {
+                // TODO: no manual pass userId later
+                const updatedEvent = await EventService.updateEvent(request.params.eventId, request.params.userId, request.body);
+                if (!updatedEvent) {
+                    return reply.status(500).send({ error: "Fail to update event." });
+                }
+                return reply.status(200).send(updatedEvent);
+            } catch (error) {
+                return reply.status(500).send({ error: "Fail to update event. "});
+            }
         },
     );
 }
