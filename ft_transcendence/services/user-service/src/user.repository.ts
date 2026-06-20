8222283f6a0259390366a6d5396f224f490171cb
ewu <erya.wuu@gmail.com>
2026-06-20 12:10:13 +0200

    feat: add data repo for user service, modify user data structure.

---
+++ ft_transcendence/services/user-service/src/user.repository.ts
@@ -0,0 +1,4 @@
+/**
+ * add middle layer to handle the database operations
+ * easier to switch to real db
+ */
--- ft_transcendence/services/user-service/src/users.types.ts
+++ ft_transcendence/services/user-service/src/users.types.ts
@@ -3,17 +3,32 @@
  * now for mocking usage
  */
 
-// internal user interface with all necessary data
-export interface UserDTO {
-    id: string;
-    intraName: string;
-    email: string;
-    intraUrl: string;
-    createdAt: string;
-}
-
 // frontend visible user's data
 export interface UserProfile {
-    intraName: string;
-    intraUrl: string;
+    userName: string;
+    userEmail: string;
+    // opional phone number
+    userContact?: string;
+    intraName?: string;
+    intraUrl?: string;
+}
+
+// internal user interface with all necessary data, this will be the payload
+export interface InternalUserEntity extends UserProfile {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+// create user
+export interface CreateUserDTO {
+    userName: string;
+    userEmail: string;
+    userContact?: string;
+}
+
+export interface UpdateUserDTO {
+    userName?: string;
+    userEmail?: string;
+    userContact?: string;
 }
