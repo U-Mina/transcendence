91e09884a9cfb51763481426257ef04b95ee4c04
ewu <erya.wuu@gmail.com>
2026-06-28 12:00:43 +0200

    fix: fix the repeated return of result.

--- ft_transcendence/apps/api-gateway/src/services/proxy.service.ts
+++ ft_transcendence/apps/api-gateway/src/services/proxy.service.ts
@@ -21,14 +21,14 @@ export async function proxyToService(
     url: string,
     body?: unknown,
     headers?: Record<string, string>
-): Promise<ProxyResult | undefined> {
+): Promise<ProxyResult> {
     // put in try catch block, maybe remove later(?)
     try {
         // this avoid pass body empty case (undefined) to fecth() which will cause ts error
         const requestInit: RequestInit = {
             method: method,
             headers: {
-                "Content-Type": "application/json",
+                "content-type": "application/json",
                 ...headers,
             }
         }
@@ -38,15 +38,13 @@ export async function proxyToService(
 
         // call internal service with fetch(), by default method is GET
         const response = await fetch(url, requestInit);
-
         // read response body, convert to json
         const result = await response.json();
 
-        if (!response.ok) {
-            return {
-                statusCode: response.status,
-                body: result
-            }
+        // even any error occurs, such as !response.ok, simply return '!ok status'
+        return {
+            statusCode: response.status,
+            body: result
         }
     } catch (error) {
         return {
