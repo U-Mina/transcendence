d78f828a468dac7ce3c762faa3d4cbc022524a73
ewu <erya.wuu@gmail.com>
2026-06-28 13:24:03 +0200

    feat: minimal getter in api-gateway to get event (connected with user alredy).

---
+++ ft_transcendence/apps/api-gateway/src/routes/event.routes.ts
@@ -0,0 +1,28 @@
+import { type FastifyInstance, type FastifyRequest, type FastifyReply, fastify } from "fastify";
+import { proxyToService } from "../services/proxy.service.js";
+
+const EVENT_SERVICE_URL = process.env.EVENT_SERVICE_URL ?? "http://localhost:3002";
+
+export async function EventGatewayRoutes(fastify: FastifyInstance) {
+    // get event by id
+    fastify.get(
+        "/events/:eventId",
+        async (
+            request: FastifyRequest<{
+                Params: { eventId: string }
+            }>,
+            reply: FastifyReply,
+        ) => {
+            const { eventId } = request.params;
+            const result = await proxyToService(
+                "GET",
+                EVENT_SERVICE_URL + "/events" + eventId,
+                undefined,
+                {
+                    "x-user": request.headers["x-user"] as string
+                }
+            );
+            return reply.status(result.statusCode).send(result.body);
+        }
+    );
+}
