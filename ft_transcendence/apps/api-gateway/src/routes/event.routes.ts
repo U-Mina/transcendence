6fcd246918895b74d057b249c336f66341093072
ewu <erya.wuu@gmail.com>
2026-06-29 20:34:31 +0200

    feat: write the delete, update and create proxy .

--- ft_transcendence/apps/api-gateway/src/routes/event.routes.ts
+++ ft_transcendence/apps/api-gateway/src/routes/event.routes.ts
@@ -1,6 +1,8 @@
 import { type FastifyInstance, type FastifyRequest, type FastifyReply } from "fastify";
 import { proxyToService } from "../services/proxy.service";
 import { authMiddleware } from "../middleware/auth.middleware";
+// NOTE: if import the data-type, it is against microservice
+// import type { CreateEventDTO, UpdateEventDTO } from "../../../../services/event-service/src/event.types";
 
 const EVENT_SERVICE_URL = process.env.EVENT_SERVICE_URL ?? "http://localhost:3002";
 
@@ -45,6 +47,75 @@ export async function eventGatewayRoutes(fastify: FastifyInstance) {
                     "x-user": request.headers["x-user"] as string
                 }
             );
+            return reply.status(result.statusCode).send(result.body);
+        }
+    );
+
+    // to create new event, the body must createEventDTO
+    fastify.post(
+        "/events",
+        async (
+            request: FastifyRequest<{
+                Body: unknown,
+            }>,
+            reply: FastifyReply,
+        ) => {
+            const result = await proxyToService(
+                "POST",
+                `${EVENT_SERVICE_URL}/events`,
+                request.body,
+                {
+                    "x-user": request.headers["x-user"] as string
+                }
+            );
+            return reply.status(result.statusCode).send(result.body);
+        }
+    );
+
+    // to delete event
+    fastify.delete(
+        "/events/:eventId",
+        async (
+            request: FastifyRequest<{
+                Params: { eventId: string },
+            }>,
+            reply: FastifyReply,
+        ) => {
+            const { eventId } = request.params;
+            const result = await proxyToService(
+                "DELETE",
+                `${EVENT_SERVICE_URL}/events/${eventId}`,
+                undefined,
+                {
+                    "x-user": request.headers["x-user"] as string
+                }
+            );
+            return reply.status(result.statusCode).send(result.body);
+        }
+    );
+
+    // updating
+    fastify.put(
+        "/events/:eventId",
+        async (
+            request: FastifyRequest<{
+                Params: {
+                    eventId: string,
+                },
+                Body: unknown,
+            }>,
+            reply: FastifyReply,
+        ) => {
+            const { eventId } = request.params;
+            const result = await proxyToService(
+                "PUT",
+                `${EVENT_SERVICE_URL}/events/${eventId}`,
+                request.body,
+                {
+                    "x-user": request.headers["x-user"] as string
+                }
+            );
+
             return reply.status(result.statusCode).send(result.body);
         }
     );
