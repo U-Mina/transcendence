4ca5fb1d77646bff59a2294c4bc243d78ebd1988
pdrettas <pdrettas@student.42heilbronn.com>
2026-07-05 23:08:05 +0200

    REFACTOR(frontend): move events fetch call into dedicated services file && add date conversion

---
+++ ft_transcendence/frontend/src/services/events/events.ts
@@ -0,0 +1,20 @@
+// this is where all the fetch stuff will go for the endpoints (event-related API calls)
+// backend request done here & then called in EventsPage
+
+import type { EventCard } from "../../types/event";
+
+export async function getEvents(): Promise<EventCard[]> { // ...
+    const response = await fetch("/events"); // ...
+
+    if (!response.ok)
+        throw new Error("Error: Failed to fetch events");
+
+    const data = await response.json();
+
+    // date conversion needed (bc cannot return response right away)
+    return data.map((event: EventCard & { startTime: string; endTime: string }) => ({
+        ...event,
+        startTime: new Date(event.startTime),
+        endTime: new Date(event.endTime),
+    }));
+}
