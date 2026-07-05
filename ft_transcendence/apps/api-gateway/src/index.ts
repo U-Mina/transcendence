0f8186498eed84dc3206455e976b64cbdbbc06d3
ewu <erya.wuu@gmail.com>
2026-07-05 12:14:47 +0200

    feat: implement swagger ui dashboard and api spec.

--- ft_transcendence/apps/api-gateway/src/index.ts
+++ ft_transcendence/apps/api-gateway/src/index.ts
@@ -1,6 +1,8 @@
 /**
  * the ONLY connecting point with outside world
  */
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
 import { healthCheckRoutes } from "./routes/health.routes";
 import { eventGatewayRoutes } from "./routes/event.routes";
 import { UserGatewayRoutes } from "./routes/user.routes";
@@ -11,7 +13,30 @@ const fastify = Fastify({
     logger: true,
 });
 
+/**
+ * IMPORTANT: register swagger and swaggerUI before register routes
+*/
+
 const start = async () => {
+    // register swagger generator
+    fastify.register(swagger, {
+        openapi: {
+            openapi: "3.0.0.0",
+            info: {
+                title: "Transcendence API",
+                description: "API documentation for the Transcendence social-media web application",
+                version: "1.0.0"
+            },
+            servers: [{ url: "http://localhost:3000" }],
+            tags: [ {name: "auth"}, {name: "system"}, {name: "events"}, {name: "users"} ]
+        }
+    });
+    
+    // register swagger ui, the 'localhost:3000/docs is the interactive UI web
+    fastify.register(swaggerUI, {
+        routePrefix: '/docs',
+    });
+
     fastify.register(healthCheckRoutes);
     fastify.register(eventGatewayRoutes, { prefix: "/api/v1" });
     fastify.register(UserGatewayRoutes, { prefix: "/api/v1" });
