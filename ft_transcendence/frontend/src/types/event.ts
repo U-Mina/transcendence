35193f9f72c133c07d7172364c76e3b10358b720
pdrettas <pdrettas@student.42heilbronn.com>
2026-07-08 02:40:55 +0200

    FEAT(frontend): copy interfaces from backend event types into frontend

--- ft_transcendence/frontend/src/types/event.ts
+++ ft_transcendence/frontend/src/types/event.ts
@@ -12,3 +12,25 @@ export interface EventCard {
     // comments / questins session
     comment?: string;
 }
+
+// the event view for creator of the card ONLY
+export interface EventManageView extends EventCard {
+    creatorId: string;
+    // time stamp
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+// user-info object embeded in the detailed event-view
+export interface UserSummary {
+    // userId: string;
+    userName: string;
+    intraName?: string;
+    intraUrl?: string;
+}
+
+export interface EventDetailView extends EventCard {
+    creator: UserSummary;
+}
+
+// TODO: isnt this sth i should just put the path (services/event-service/src/event.types.ts) from backend into my event.ts
