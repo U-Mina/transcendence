034527645be276f280dd8ec2cf9d012261eeaa78
ewu <erya.wuu@gmail.com>
2026-07-20 19:00:59 +0200

    feat: 1. declaritive file for img url and jwt; 2. update package json.

--- ft_transcendence/apps/api-gateway/src/types/fastify.d.ts
+++ ft_transcendence/apps/api-gateway/src/types/fastify.d.ts
@@ -1,7 +1,14 @@
 import "fastify";
+import "@fastify/jwt";
 
 declare module "fastify" {
     interface FastifyRequest {
         metricsStartTime?: bigint;
+    }
+}
+
+declare module "@fastify/jwt" {
+    interface FastifyJWT {
+        user: { id: string; email: string };
     }
 }
