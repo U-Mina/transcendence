ec17379a72a3e4f1af2877ae92d57c46b22abf71
Dariusz Paluszkiewicz <dpaluszk@2-C-4.42heilbronn.de>
2026-07-21 21:48:19 +0200

    Started writing a script for creating a root ca authority, started by creating multiple variables and genereting self signed root ca certificate.

--- ft_transcendence/scripts/generate-certificates.sh
+++ ft_transcendence/scripts/generate-certificates.sh
@@ -4,3 +4,27 @@ set -eu
 
 SCRIPT_DIR="$(cd "$(dirname "$0)" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+CERT_DIR="$(cd "$PROJECT_ROOT/.certificates)
+CA_DIR="$CERT_ROOT/ca"
+CA_KEY="$CA_DIR/ca.key"
+CA_CERT="$CA_DIR/ca.crt"
+
+mkdir -p "$CA_DIR"
+
+if [ ! -f "CA_KEY] || [ ! -f "CA_CART" ]; then
+    openssl req \
+        -x509 \
+        -new \
+        -newkey rsa:4096 \
+        -nodes \
+        -sha256 \
+        -days 3650 \
+        -keyout "$CA_KEY" \
+        -out "$CA_CERT" \
+        -subj "/CN=Transcendence Local CA" \
+        -addext "basicConstraints=critical,CA:TRUE" \
+        -addext "keyUsage=critical,keyCertSign,cRLSign"
+
+    chmod 600 "$CA_KEY"
+    chmod 644 "$CA_CERT"
+fi
