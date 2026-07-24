18a6a093ac126464bc0ad8fa5bfe660c6afa7811
ewu <erya.wuu@gmail.com>
2026-07-24 21:33:56 +0200

    chore: clean empty pages, remove repeated css.

--- ft_transcendence/frontend/src/pages/UpdateEventPage.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * need to include an update/edit button on the eventdetailspage 
- *      (but this button, only the creator of event can see). 
- * then create a page called updateEvent which includes the form and everything
- *      (this is where the edit button leads to)
- */
-
